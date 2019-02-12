58f0c04cafd1b5acf38d89ffdeaee1abd446868c
Ondrej Moravcik <ondrej.moravcik@monster.com>
2019-02-12 16:20:00 +0100

    Add test comment

--- SourceLinkNuget/Class1.cs
+++ SourceLinkNuget/Class1.cs
@@ -7,6 +7,23 @@ namespace SourceLinkNuget
         public void Test()
         {
             Console.WriteLine("test");
+            /*
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec rutrum felis a suscipit scelerisque. In justo diam, tincidunt eget justo sed, gravida vulputate turpis. Vivamus sagittis odio vulputate lacus varius, a ullamcorper lacus ornare. Praesent maximus aliquet porttitor. Nunc accumsan massa id dui hendrerit dignissim. In sit amet lectus ut metus posuere imperdiet. Donec vitae tortor id augue efficitur condimentum non in metus. Nulla facilisi. Nunc pharetra enim sed quam elementum rutrum. Proin bibendum cursus eros, eget imperdiet dolor mollis vel. Curabitur molestie, enim sed pellentesque elementum, quam felis interdum felis, eu malesuada turpis orci in odio. Donec gravida ornare est, lacinia tincidunt lorem aliquam nec. Nunc porttitor ipsum eu faucibus sagittis.
+
+Duis sit amet sodales ligula, quis tempus felis. Nullam mattis felis nisi, ac fermentum urna consequat at. Pellentesque gravida, nisi non convallis aliquam, erat arcu molestie sem, vitae eleifend ante massa efficitur nulla. Vestibulum a mattis ligula. Phasellus fringilla nunc a magna pellentesque, id sollicitudin erat semper. Aliquam lobortis leo erat, ac rhoncus velit facilisis ac. Vivamus et ligula sed turpis pellentesque luctus aliquam sed massa. Fusce consectetur sit amet ante sed luctus. Duis et mi vel lacus dignissim porttitor. Integer sit amet facilisis ipsum. Donec placerat dolor arcu, a congue lacus condimentum ut. Phasellus venenatis metus ac felis pharetra interdum. Suspendisse potenti.
+
+Curabitur velit est, condimentum sit amet sodales nec, faucibus quis magna. Nunc congue sed erat a suscipit. Nulla facilisi. Quisque imperdiet ante vitae nunc finibus, at ornare arcu venenatis. Suspendisse et nulla eget justo aliquam convallis. Etiam turpis purus, lacinia sed urna nec, porta porttitor dui. Ut ut maximus lacus. Morbi molestie dapibus augue a consectetur. Suspendisse at erat ac felis elementum auctor vel sit amet odio. Aliquam sed libero nunc. Mauris eget varius tellus. Quisque eget mauris arcu.
+
+Aenean imperdiet pretium ultrices. Nulla maximus, nisl non auctor mollis, massa nulla porta ligula, nec pretium leo neque nec nunc. Ut ornare eros a efficitur tincidunt. Curabitur vestibulum vulputate nulla vel semper. Phasellus hendrerit neque eget tortor aliquet, id auctor est consequat. Fusce nec gravida mi. Cras ornare malesuada dui, lobortis commodo lectus auctor vitae. Nam ut augue nisi. Aliquam molestie faucibus scelerisque. Maecenas ac erat quis magna sagittis tincidunt. Pellentesque pharetra vehicula aliquam. Donec blandit est odio, a eleifend turpis pretium a. Ut in diam ultrices, auctor tellus sed, mattis ligula. In leo quam, viverra vitae laoreet sit amet, elementum ac nulla. Phasellus eget tincidunt neque. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
+
+Duis lacinia ligula sed urna dictum, ac tempor ligula gravida. Nullam bibendum commodo lacus quis porttitor. Etiam in malesuada felis, bibendum pretium dui. Etiam sit amet nisi sapien. Donec magna nunc, mollis eget ante in, porta aliquet risus. Sed fermentum, augue ut tincidunt tincidunt, velit velit iaculis augue, at porta ex massa sit amet nisl. Pellentesque accumsan ac leo nec pulvinar. Integer vel nulla vel dui mollis congue nec et sem. Duis vel odio in quam fringilla efficitur. Ut vulputate euismod condimentum. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque hendrerit fringilla feugiat. Ut convallis leo leo, blandit ultricies ante pretium vel. Aliquam nec elit non magna gravida consectetur. Etiam facilisis consequat hendrerit. Aenean eget nulla dolor. Sed tempor tortor neque, id blandit nisl facilisis ac. Fusce vel pretium ligula, a pulvinar augue. Mauris quis viverra tortor.
+
+Nullam gravida tellus lectus, egestas condimentum velit molestie ac. Pellentesque eleifend, dolor sed gravida feugiat, augue purus tempor sapien, ut suscipit purus est at lectus massa nunc.
+
+Generated 7 paragraphs, 591 words, 4000 bytes of Lorem Ipsum
+*/
         }
 
         public void ThrowException()
